a5e60462ec0c8b439e913a4b869d43ca8e02add6
amir mekkaoui <amirmekk@outlook.com>
2020-06-17 02:29:51 +0100

    init user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
-  
+  final String id, username, email, photoUrl, displayName, bio;
+  User({
+    this.bio,
+    this.displayName,
+    this.email,
+    this.id,
+    this.photoUrl,
+    this.username,
+  });
+  factory User.fromDocument(DocumentSnapshot doc) {
+    return User(
+      bio: doc['bio'],
+      displayName: doc['displayName'],
+      email: doc['email'],
+      id: doc['id'],
+      photoUrl: doc['photoUrl'],
+      username: doc['username'],
+    );
+  }
 }
