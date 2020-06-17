57f1fd823c1f66e44ed2d5fefc6576dadec0447a
amir mekkaoui <amirmekk@outlook.com>
2020-06-17 02:31:42 +0100

    creating a user in my data base after the user registers with google auth + create an instance of User

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:social_network/widgets/header.dart';
 
 class CreateAccount extends StatefulWidget {
   @override
@@ -6,8 +9,94 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String username;
+  submit() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      SnackBar snackbar = SnackBar(content: Text('welcome $username'));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+      Timer(Duration(seconds: 2), () {
+        Navigator.pop(context, username);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext parentContext) {
-    return Text("Create Account");
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: header(
+        context,
+        titleText: 'Set up your profile',
+        removeBackButton: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0),
+            child: Center(
+              child: Text(
+                'Choose a username',
+                style: TextStyle(
+                  fontSize: 26,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                onSaved: (value) {
+                  setState(() => username = value);
+                },
+                autovalidate: true,
+                validator: (val) {
+                  if (val.trim().length < 3 || val.isEmpty) {
+                    return 'username too short';
+                  } else if (val.trim().length > 20) {
+                    return 'username too long';
+                  } else if (val.contains(' ')) {
+                    return 'username can\'t contain whitespace';
+                  }
+
+                  return null;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                  labelStyle: TextStyle(
+                    fontSize: 15,
+                  ),
+                  hintText: 'Must be at least 3 caracters',
+                ),
+              ),
+            ),
+          ),
+          ButtonTheme(
+            minWidth: MediaQuery.of(context).size.width / 100 * 90,
+            height: 50,
+            child: RaisedButton(
+              color: Theme.of(context).primaryColor,
+              onPressed: submit,
+              child: Text(
+                'Next',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:social_network/models/user.dart';
 import 'package:social_network/pages/activity_feed.dart';
+import 'package:social_network/pages/create_account.dart';
 import 'package:social_network/pages/profile.dart';
 import 'package:social_network/pages/search.dart';
 import 'package:social_network/pages/timeline.dart';
 import 'package:social_network/pages/upload.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
+final CollectionReference usersRef = Firestore.instance.collection('users');
+final DateTime timestamp = DateTime.now();
+User currentUser;
 
 class Home extends StatefulWidget {
   @override
@@ -40,6 +46,7 @@ class _HomeState extends State<Home> {
 
   void handleSignIn(GoogleSignInAccount account) {
     if (account != null) {
+      createUserInFirestore();
       print('my user account is  : $account');
       setState(() {
         isAuth = true;
@@ -50,6 +57,39 @@ class _HomeState extends State<Home> {
         isAuth = false;
       });
     }
+  }
+
+  createUserInFirestore() async {
+    //1- check if user exists in database(according to their ID)
+    final GoogleSignInAccount user = googleSignIn.currentUser;
+    DocumentSnapshot doc = await usersRef.document(user.id).get();
+    if (!doc.exists) {
+      //2- if user doesnt exist we want them to create account page
+      final String username = await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CreateAccount(),
+        ),
+      );
+      //3- get username from create account, use it to make new user document in users collection
+      usersRef.document(user.id).setData({
+        'id': user.id,
+        'username': username,
+        'email': user.email,
+        'photoUrl': user.photoUrl,
+        'displayName': user.displayName,
+        'bio': '',
+        'timestamp': timestamp,
+      });
+      doc = await usersRef.document(user.id).get();
+    }
+    currentUser = User.fromDocument(doc);
+    print(currentUser);
+    print(currentUser.email);
+
+    setState(() {
+      // this.currentUser = currentUser;
+    });
   }
 
   logIn() {
@@ -78,7 +118,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: PageView(
         children: <Widget>[
-          Timeline(),
+          //Timeline(),
+          RaisedButton(onPressed: logOut, child: Text('log out ')),
           ActivityFeed(),
           Upload(),
           Search(),
