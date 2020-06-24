9816c0d2f3a842e98447343d60dfccfbd547bf41
amir mekkaoui <amirmekk@outlook.com>
2020-06-24 16:53:03 +0100

    added ability to modify user profile data

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:social_network/models/user.dart';
-import 'package:social_network/pages/timeline.dart';
+import 'package:social_network/pages/edit_profile.dart';
+//import 'package:social_network/pages/timeline.dart';
+import 'package:social_network/pages/home.dart';
 import 'package:social_network/widgets/header.dart';
 import 'package:social_network/widgets/progress.dart';
 
@@ -13,6 +15,7 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser?.id;
   Column buildCountColumn(String label, int count) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -40,7 +43,50 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  editProfile() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return EditProfile(
+        currentUserId: currentUserId,
+      );
+    }));
+  }
+
+  Container buildButton({String text, Function function}) {
+    return Container(
+      padding: EdgeInsets.only(top: 2.0),
+      child: FlatButton(
+        onPressed: function,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 75),
+          height: 27.0,
+          //width: 220,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              color: Colors.blue,
+            ),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+
   buildProfileButton() {
+    //check if we're viewing our own profile! => if so show edit profile button
+    bool isProfileOwner = currentUserId == widget.profileId;
+    if (isProfileOwner) {
+      return buildButton(text: 'Edit Profile', function: editProfile);
+    }
     return Text('profile button');
   }
 
